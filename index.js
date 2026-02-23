f10448283a56290dbf527854adb80e2d67157066
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-23 22:17:07 +0600

    Updating user's lastSignInTime using a PATCH API

--- index.js
+++ index.js
@@ -77,6 +77,18 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/user', async (req, res) => {
+            const { email, lastSignInTime } = req.body;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {
+                    lastSignInTime: lastSignInTime
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
         app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
